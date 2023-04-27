3e479e356c9b421703f42fe98e50b1ef2bf6b2f2
endlessmomo <soohyuk96@naver.com>
2023-04-27 21:01:34 +0900

    feat(CardController) : Commands에 따른 요청을 처리하는 컨트롤러 구현체 생성

---
+++ src/main/java/controller/CardController.java
@@ -0,0 +1,73 @@
+package controller;
+
+import domain.Commands;
+import domain.dto.CommandDto;
+import global.Retry;
+import main.view.InputView;
+import main.view.OutputView;
+import service.CardService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CardController {
+    private Map <Commands,Runnable> service = new HashMap <>();
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private CardService cardService;
+
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+        service.put(Commands.ISSUE, this::);
+        service.put(Commands.DEPOSIT_AND_WITHDRAWAL, this::);
+        service.put(Commands.PAYED, this::);
+        service.put(Commands.RECORD, this::);
+        service.put(Commands.RE_ISSUE, this::);
+    }
+
+    public void run() {
+        Commands command = readCommand();
+        while (command.isNotQuit()) {
+            try{
+                service.get(command).run();
+            } catch (IllegalArgumentException e){
+            }
+            command = readCommand();
+        }
+    }
+
+    // 카드 발급 API
+    public void issue() {
+
+    }
+
+    // 카드 입금/출금 API
+    public void depositAndWithdrawal() {
+
+    }
+
+    // 결제 API
+    public void payed() {
+
+    }
+
+    // 카드 거래 내역 확인 API
+    public void record() {
+
+    }
+
+    // 카드 재발급 API
+    public void reIssue() {
+
+    }
+
+    public Commands readCommand() {
+        try {
+            CommandDto dto = Retry.execute(inputView::readCommand);
+            return Commands.from(dto.getCommand());
+        } catch (IllegalArgumentException e) {
+             outputView.printException(e.getMessage());
+        }
+        return null;
+    }
+}
